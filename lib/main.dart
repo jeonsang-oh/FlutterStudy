5c2c2dc84b9432a36cb0bf88480e9e80730d8505
jeonsang-oh <jeon.sangoh92@gmail.com>
2024-08-13 06:54:45 +0900

    flutter01 - hello World

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Welcome to Flutter',
+          ),
+        ),
+        body: Center(
+          child: Text(
+            'Hello World!',
+            style: GoogleFonts.aladin(
+              fontSize: 48,
+              fontWeight: FontWeight.w700,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+        ),
+      )
+    );
+  }
+}
