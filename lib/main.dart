26b19e764c95752a525804f4c7d79fd7f5d21692
jeonsang-oh <jeon.sangoh92@gmail.com>
2025-04-13 14:21:12 +0900

    flutter01 - hello World / appbar, 위젯 중간 표시, 아이콘 등록

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+// 화면에 보여줄 웨짓을 지정, main 함수를 시작으로 runApp() 함수를 실행해 플러터 프로젝트를 시작한다.
 void main() {
   runApp(const MyApp());
 }
 
+// 상태 변화가 없는 앱을 만든다.
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -13,22 +15,42 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
+        textTheme: GoogleFonts.aladinTextTheme(
+          Theme.of(context).textTheme,
+        ),
+        useMaterial3: false,
         primarySwatch: Colors.green,
       ),
       home: Scaffold(
         appBar: AppBar(
           title: Text(
             'Welcome to Flutter',
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
           ),
+          actions: [
+            Icon(Icons.shopping_cart), // 쇼핑카트
+            SizedBox(width: 10), // 위젯간 여백 조정
+            Icon(Icons.search_rounded, // 검색아이콘
+                color: Colors.redAccent),
+            SizedBox(width: 10),
+            Icon(Icons.settings, // 설정 아이콘
+                color: Colors.purpleAccent),
+            SizedBox(width: 10),
+          ],
         ),
         body: Center(
           child: Text(
-            'Hello World!',
-            style: GoogleFonts.aladin(
-              fontSize: 48,
-              fontWeight: FontWeight.w700,
-              fontStyle: FontStyle.italic,
-            ),
+              'Hello World',
+              style: TextStyle(
+                fontSize: 48,
+                fontWeight: FontWeight.w700,
+                fontStyle: FontStyle.italic,
+                color: Colors.purple,
+              ),
           ),
         ),
       )
